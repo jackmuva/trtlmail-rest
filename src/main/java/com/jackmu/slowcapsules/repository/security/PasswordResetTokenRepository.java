59edaf35122cb3692dbb46f8e68fe64dead5c707
jackmuva <33766184+jackmuva@users.noreply.github.com>
2024-06-23 11:16:52 -0700

    removed repository annotation

--- src/main/java/com/jackmu/slowcapsules/repository/security/PasswordResetTokenRepository.java
+++ src/main/java/com/jackmu/slowcapsules/repository/security/PasswordResetTokenRepository.java
@@ -5,12 +5,10 @@ import com.jackmu.slowcapsules.model.security.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Date;
 import java.util.List;
 
-@Repository
 public interface PasswordResetTokenRepository extends JpaRepository<PasswordResetToken, Long> {
 
     PasswordResetToken findByToken(String token);
